eaf2692e0e69d10b6f9318380737756656f661eb
Ryan Panning <panman82@gmail.com>
2016-03-02 14:52:15 -0600

    Add normalizeEntityName() to the blueprint
    The normalizeEntityName() is required for the addon’s default
    blueprint, or else you’ll get an “entity-name” error when trying to
    `ember install`

--- blueprints/ember-froala-editor/index.js
+++ blueprints/ember-froala-editor/index.js
@@ -2,7 +2,13 @@
 var RSVP = require('rsvp');
 
 module.exports = {
-  description: '',
+  description: 'Add bower dependency for froala-wysiwyg-editor and also a project dep for ember-cli-font-awesome',
+
+  normalizeEntityName: function() {
+    // allows us to run ember -g ember-bootstrap-switch and not blow up
+    // because ember cli normally expects the format
+    // ember generate <entitiyName> <blueprint>
+  }, // :normalizeEntityName
 
   afterInstall: function() {
     return RSVP.all([
