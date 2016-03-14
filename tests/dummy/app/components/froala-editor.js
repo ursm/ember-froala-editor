12d4733d6e3585328d76ab5032cf9b05331ce758
Ryan Panning <panman82@gmail.com>
2016-03-14 13:21:47 -0500

    Add key for documentation Website

---
+++ tests/dummy/app/components/froala-editor.js
@@ -0,0 +1,7 @@
+import FroalaEditorComponent from 'ember-froala-editor/components/froala-editor';
+
+export default FroalaEditorComponent.extend({
+  defaultOptions: {
+    key: '7D-11nrC-7saC5ejpmseD3c1e=='
+  }
+});
--- tests/dummy/app/controllers/index.js
+++ tests/dummy/app/controllers/index.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   exampleOptions: {
-    height: 100,
+    height: 300,
     toolbarButtons: ['fullscreen', 'bold', 'italic', 'underline', 'strikeThrough', 'subscript', 'superscript', 'fontFamily', 'fontSize', '|', 'color', 'emoticons', '|', 'paragraphFormat', 'align', 'formatOL', 'formatUL', 'outdent', 'indent', 'quote', 'insertHR', '-', 'insertLink', 'insertImage', 'insertVideo', 'insertFile', 'insertTable', 'undo', 'redo', 'clearFormatting', 'selectAll', 'html'],
     toolbarButtonsMD: ['fullscreen', 'bold', 'italic', 'underline', 'fontFamily', 'fontSize', 'color', 'paragraphStyle', 'paragraphFormat', 'align', 'formatOL', 'formatUL', 'outdent', 'indent', '-', 'quote', 'insertHR', 'insertLink', 'insertImage', 'insertVideo', 'insertFile', 'insertTable', 'undo', 'redo', 'clearFormatting']
   }
