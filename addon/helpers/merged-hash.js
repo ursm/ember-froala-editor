efba5c0b628b99d9ff7c4209a2f6a2df7ade2c92
Ryan Panning <panman82@gmail.com>
2016-11-01 15:11:04 -0500

    Have the `{{merged-hash}}` helper use the `assign` polyfill

--- addon/helpers/merged-hash.js
+++ addon/helpers/merged-hash.js
@@ -1,24 +1,11 @@
 import Ember from 'ember';
+import assign from 'ember-assign-polyfill';
 
 
 export function mergedHash( params, hash ) {
   // Assumes all params are objects
   if ( params.length > 0 ) {
-    let merged = {};
-
-    // Use Ember.assign() if available (added in ember v2.5)
-    if ( Ember.assign ) {
-      merged = Ember.assign( merged, ...params, hash );
-
-    // Fall back to Ember.merge() (depreciated as of ember v2.5)
-    } else {
-      params.forEach( param => {
-        merged = Ember.merge( merged, param );
-      });
-      merged = Ember.merge( merged, hash );
-    }
-
-    return merged;
+    return assign( {}, ...params, hash );
   } else {
     return hash;
   }
