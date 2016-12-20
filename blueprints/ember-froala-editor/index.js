516ab5b932a6004b154a0b498e35f9743fec179f
Ryan Panning <panman82@gmail.com>
2016-12-20 08:47:45 -0600

    Blueprint should add ember-font-awesome
    This was somehow missed among all the other updates… Fixes #26

--- blueprints/ember-froala-editor/index.js
+++ blueprints/ember-froala-editor/index.js
@@ -2,7 +2,7 @@
 var RSVP = require('rsvp');
 
 module.exports = {
-  description: 'Add bower dependency for froala-wysiwyg-editor and also a project dep for ember-cli-font-awesome',
+  description: 'Add bower dependency for froala-wysiwyg-editor and also a project dep for ember-font-awesome',
 
   normalizeEntityName: function() {
     // allows us to run ember -g ember-bootstrap-switch and not blow up
@@ -13,7 +13,7 @@ module.exports = {
   afterInstall: function() {
     return RSVP.all([
       this.addBowerPackageToProject( 'froala-wysiwyg-editor',  '~2.4.0' ),
-      this.addAddonToProject(        'ember-cli-font-awesome', '^1.5.0' )
+      this.addAddonToProject(        'ember-font-awesome', '^2.2.0' )
     ]);
   } // :afterInstall
 
