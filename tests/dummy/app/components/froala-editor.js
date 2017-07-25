c2123b2efc4ce0a70d84dd3150c15ce83bd4df5a
Ryan Panning <panman82@gmail.com>
2017-07-24 19:49:33 -0500

    Update editor key for new docs domain

--- tests/dummy/app/components/froala-editor.js
+++ tests/dummy/app/components/froala-editor.js
@@ -2,6 +2,6 @@ import FroalaEditorComponent from 'ember-froala-editor/components/froala-editor'
 
 export default FroalaEditorComponent.extend({
   options: {
-    key: '7D-11nrC-7saC5ejpmseD3c1e=='
+    key: 'Tvywseoh1irkdyzdhvlvC8ehe=='
   }
 });
