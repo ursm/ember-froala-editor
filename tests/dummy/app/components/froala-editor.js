c8e2d1ac9ed72967aa8f8210f8c05f2af8044cab
Panman8201 <panman82@gmail.com>
2018-05-04 08:37:37 -0500

    Update editor key for docs site

--- tests/dummy/app/components/froala-editor.js
+++ tests/dummy/app/components/froala-editor.js
@@ -3,6 +3,6 @@ import FroalaEditorComponent from 'ember-froala-editor/components/froala-editor'
 export default FroalaEditorComponent.extend({
   // eslint-disable-next-line ember/avoid-leaking-state-in-ember-objects
   options: {
-    key: 'Tvywseoh1irkdyzdhvlvC8ehe=='
+    key: '3C3B6eF5B4A3E3E2C2C6D6A3D3xg1kemB-22B-16rF-11dyvwtvze1zdA1H-8vw=='
   }
 });
