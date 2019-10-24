da40318ff1dc85cf547fb3bc29b7e17fc52ea1f9
Panman8201 <panman82@gmail.com>
2019-10-24 14:52:04 -0500

    Change {{froala-html}} to require a callback/setter

--- addon/helpers/froala-html.js
+++ addon/helpers/froala-html.js
@@ -1,14 +1,31 @@
 import { helper } from '@ember/component/helper';
 import { assert } from '@ember/debug';
+import { htmlSafe } from '@ember/template';
 
-export function froalaHtml([editor]/*, hash*/) {
+export function froalaHtml([callback, ...partial]/*, hash*/) {
   assert(
-    '{{froala-html}} helper cannot determine the editor instance',
-    (editor && editor.html && typeof editor.html.get === 'function') ||
-    (this && this.html && typeof this.html.get === 'function')
+    '{{froala-html}} helper requires a function as the first parameter',
+    typeof callback === 'function'
   );
-  let html = editor && editor.html || this.html;
-  return html.get();
+  return function returnHtml(...args) {
+    // Editor might be passed in as the first arg if also wrapped with {{froala-arg}}
+    // Note: Shift editor off args so it isn't "double passed" when args are spread on the callback
+    let editor = (typeof args[0] === 'object' ? args.shift() : this);
+
+    assert(
+      '{{froala-html}} helper cannot determine the editor instance',
+      (editor.html && typeof editor.html.get === 'function')
+    );
+
+    // The ember component knows if the @content in isHTMLSafe() or not
+    let returnSafeString = (editor.emberComponent && editor.emberComponent.returnSafeString);
+
+    // Get the html content from the editor, making it a SafeString if needed
+    let html = (returnSafeString ? htmlSafe(editor.html.get()) : editor.html.get());
+
+    // Call the setter, passing in the html first then all others
+    return callback(html, editor, ...partial, ...args);
+  };
 }
 
 export default helper(froalaHtml);
--- tests/integration/helpers/froala-html-test.js
+++ tests/integration/helpers/froala-html-test.js
@@ -1,51 +1,117 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
-import hbs from 'htmlbars-inline-precompile';
 import { froalaHtml } from 'ember-froala-editor/helpers/froala-html';
+import { isHTMLSafe } from '@ember/template';
 
 module('Integration | Helper | froala-html', function(hooks) {
   setupRenderingTest(hooks);
 
 
-  test('rendered helper with explicitly passed in editor', async function(assert) {
-    this.set('editor', { // Mock an editor instance
-      html: {
-        get() {
-          return '<p>Foobar</p>';
-        }
-      }
-    });
-    await render(hbs`{{froala-html editor}}`);
-    assert.equal(this.element.textContent.trim(), '<p>Foobar</p>');
-  });
-
-
   test('helper function called with explicitly passed in editor', function(assert) {
+    let html = '<p>Foobar</p>';
     let editor = { // Mock an editor instance
       html: {
         get() {
-          return '<p>Foobar</p>';
+          return html;
         }
       }
     };
-    assert.equal(froalaHtml([editor]), '<p>Foobar</p>');
+    let setter = content => assert.equal(content, html);
+    let callback = froalaHtml([setter]);
+    callback(editor);
   });
 
 
   test('helper function called with called context as the editor', function(assert) {
+    let html = '<p>Foobar</p>';
     let editor = { // Mock an editor instance
       html: {
         get() {
-          return '<p>Foobar</p>';
+          return html;
         }
       }
     };
-    assert.equal(froalaHtml.bind(editor)([]), '<p>Foobar</p>');
+    let setter = content => assert.equal(content, html);
+    let callback = froalaHtml([setter]).bind(editor);
+    callback();
+  });
+
+
+  test('additional arguments passed into the helper are available to the setter', function(assert) {
+    let html = '<p>Foobar</p>';
+    let editor = { // Mock an editor instance
+      html: {
+        get() {
+          return html;
+        }
+      }
+    };
+    let setter = (content, editor, htmlHere) => assert.equal(htmlHere, html);
+    let callback = froalaHtml([setter, html]);
+    callback(editor);
+  });
+
+
+  test('additional arguments passed into the callback are available to the setter', function(assert) {
+    let html = '<p>Foobar</p>';
+    let editor = { // Mock an editor instance
+      html: {
+        get() {
+          return html;
+        }
+      }
+    };
+    let setter = (content, editor, htmlHere) => assert.equal(htmlHere, html);
+    let callback = froalaHtml([setter]);
+    callback(editor, html);
+  });
+
+
+  test('additional arguments passed into the helper and callback are available to the setter', function(assert) {
+    assert.expect(2);
+    let html = '<p>Foobar</p>';
+    let editor = { // Mock an editor instance
+      html: {
+        get() {
+          return html;
+        }
+      }
+    };
+    let setter = (content, editor, check1, check2) => {
+      assert.equal(check1, html);
+      assert.equal(check2, html);
+    };
+    let callback = froalaHtml([setter, html]);
+    callback(editor, html);
+  });
+
+
+  test('helper wraps the html in a SafeString when the component returnSafeString is true', function(assert) {
+    let html = '<p>Foobar</p>';
+    let editor = { // Mock an editor instance
+      emberComponent: {
+        returnSafeString: true
+      },
+      html: {
+        get() {
+          return html;
+        }
+      }
+    };
+    let setter = content => assert.ok(isHTMLSafe(content));
+    let callback = froalaHtml([setter]);
+    callback(editor);
   });
 
 
   test('helper function called without any editor asserts', function(assert) {
+    let setter = () => {};
+    let callback = froalaHtml([setter]);
+    assert.throws(() => callback());
+  });
+
+
+  test('helper function called without any callback asserts', function(assert) {
     assert.throws(() => froalaHtml([]));
   });
 
