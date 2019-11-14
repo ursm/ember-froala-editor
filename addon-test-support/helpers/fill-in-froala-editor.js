b3a6be273d38cace10f4049bc744fadf5db3caf1
Panman8201 <panman82@gmail.com>
2019-11-14 16:38:47 -0600

    Remove Froala Editor integration examples from test helpers
    I mainly wanted them in there for posterity sake, in case we need to come back around to that approach.

--- addon-test-support/helpers/fill-in-froala-editor.js
+++ addon-test-support/helpers/fill-in-froala-editor.js
@@ -1,8 +1,6 @@
 import { isHTMLSafe } from '@ember/template';
 import { registerAsyncHelper } from '@ember/test';
 import { find, fillIn } from '@ember/test-helpers';
-// import { find, settled } from '@ember/test-helpers';
-// import FroalaEditor from 'froala-editor';
 
 export default registerAsyncHelper('fillInFroalaEditor', function(app, selector, html) {
   return fillInFroalaEditor(selector, html);
@@ -24,32 +22,5 @@ export async function fillInFroalaEditor(selector, html) {
   );
 
   return fillIn(element, html);
-
-  // let element = find(selector);
-
-  // if (element === null) {
-  //     throw `fillInFroalaEditor(): DOM element not found with the selector '${selector}'`;
-  // }
-
-  // let editor = FroalaEditor.INSTANCES.find(instance => {
-  //   if (instance['$oel'][0] === element) {
-  //     return instance;
-  //   }
-  // });
-
-  // html = (
-  //   isHTMLSafe(html) ?
-  //   html.toString() :
-  //   html
-  // );
-
-  // if (editor) {
-  //   editor.html.set(html);
-  //   editor.undo.saveStep(); // Triggers contentChanged
-  // } else {
-  //   element.innerHTML = html; // TODO: Trigger input event
-  // }
-
-  // return settled();
 
 }
--- addon-test-support/helpers/get-in-froala-editor.js
+++ addon-test-support/helpers/get-in-froala-editor.js
@@ -1,6 +1,5 @@
 import { registerHelper } from '@ember/test';
 import { find } from '@ember/test-helpers';
-// import FroalaEditor from 'froala-editor';
 
 export default registerHelper('getInFroalaEditor', function(app, selector) {
   return getInFroalaEditor(selector);
@@ -15,23 +14,5 @@ export function getInFroalaEditor(selector) {
   }
 
   return element.innerHTML.trim();
-
-  // let element = find(selector);
-
-  // if (element === null) {
-  //     throw `getInFroalaEditor(): DOM element not found with the selector '${selector}'`;
-  // }
-
-  // let editor = FroalaEditor.INSTANCES.find(instance => {
-  //   if (instance['$oel'][0] === element) {
-  //     return instance;
-  //   }
-  // });
-
-  // return(
-  //   editor ?
-  //   editor.html.get() :
-  //   element.innerHTML.trim()
-  // );
 
 }
